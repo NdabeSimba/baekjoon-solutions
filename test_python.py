0b72bdd4e1cf41ae48ad3f796544c5ce2b93cefc
Juhun Park <riann3207@gmail.com>
2023-06-06 00:26:43 +0900

    sorting by age

--- test_python.py
+++ test_python.py
@@ -1,1 +1,7 @@
-N, K = map(int, input().split())
+N = int(input())
+key = []
+value = []
+for i in range(N):
+    key_inp, value_inp = map(str, input().split())
+    key.append(key_inp)
+    value.append(value_inp)
