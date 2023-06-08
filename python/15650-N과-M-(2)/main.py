b79b34187152360ccd926e6532850d03b140e2f5
Juhun Park <riann3207@gmail.com>
2023-06-08 14:01:49 +0900

    15650, 15652

--- python/15650-N과-M-(2)/main.py
+++ python/15650-N과-M-(2)/main.py
@@ -3,7 +3,7 @@ import itertools
 N, M = map(int, input().split())
 
 arr = [i + 1 for i in range(N)]
-nCr = itertools.combinations_with_replacement(arr, M)
+nCr = itertools.combinations(arr, M)
 
 for i in nCr:
     print(*i)
