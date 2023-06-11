d18c6b2188761443d4bcb5d2449698426df1ed9d
Juhun Park <riann3207@gmail.com>
2023-06-11 22:09:11 +0900

    test

--- test_python.py
+++ test_python.py
@@ -1,23 +1,5 @@
 import sys
 input = sys.stdin.readline
 
-K, N = map(int, input().split())
-length = [int(input()) for _ in range(K)]
-
-left, right = 1, max(length)
-result = 0
-
-while left <= right:
-    mid = (left + right) // 2
-    total = 0
-
-    for leng in length:
-        total += leng // mid
-
-    if total >= N:
-        left = mid + 1
-        result = mid
-    else:
-        right = mid - 1
-
-print(result)
+N, M = map(int, input().split())
+print(abs(N - M))
