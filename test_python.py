716d8b6d277022f0e7e73be597f1276117093b87
Juhun Park <riann3207@gmail.com>
2023-06-21 17:30:29 +0900

    11367

--- test_python.py
+++ test_python.py
@@ -1,8 +1,23 @@
-name = {
-    "NLCS" : "North London Collegiate School",
-    "BHA" : "Branksome Hall Asia",
-    "KIS" : "Korea International School",
-    "SJA" : "St. Johnsbury Academy"
-        }
+def grade(n):
+    if n in [i for i in range(97, 101)]:
+        return "A+"
+    elif n in [i for i in range(90, 97)]:
+        return "A"
+    elif n in [i for i in range(87, 90)]:
+        return "B+"
+    elif n in [i for i in range(80, 87)]:
+        return "B" 
+    elif n in [i for i in range(77, 80)]:
+        return "C+"
+    elif n in [i for i in range(70, 77)]:
+        return "C" 
+    elif n in [i for i in range(67, 70)]:
+        return "D+"
+    elif n in [i for i in range(60, 67)]:
+        return "D" 
+    else:
+        return "F"
 
-print(name[input()])
+for _ in range(int(input())):
+    name, score = map(str, input().split())
+    print(name, grade(int(score)))
