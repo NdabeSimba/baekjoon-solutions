94c54b8a9cc11c1d4696729daca3e31caf223f3e
Juhun Park <riann3207@gmail.com>
2023-06-22 22:28:33 +0900

    test

--- test_python.py
+++ test_python.py
@@ -1,15 +1,32 @@
 import sys
+sys.setrecursionlimit(10000)
 
-for line in sys.stdin:
-    y1, y2, af = map(int, line.split())
+input = sys.stdin.readline
 
-    if af == 0:
-        break
+T = int(input())
 
-    if y1 == y2 == 0:
-        print(0)
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def DFS(x, y):
+    if x <= -1 or x >= N or y <= -1 or y > M:
+        return False
+    
+    if graph[x][y] == 1:
+        graph[x][y] = 0
+
+        for i in range(4):
+            DFS(x + dx[i], y + dy[i])
         
-    else:
-        for _ in range(af):
-            y1, y2 = y2, y1 + y2
-        print(y2)
+        return True
+    return False
+
+answer = []
+for _ in range(T):
+    M, N, K = list(map(int, input().split()))
+
+    graph = [[0] * M for _ in range(N)]
+
+    for _ in range(K):
+        x, y = map(int, input().split())
+        graph[y][x] = 1
