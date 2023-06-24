4f2636f90ad4e203c55122d9d455df12c51cd2d9
Juhun Park <riann3207@gmail.com>
2023-06-24 15:01:11 +0900

    1267

--- test_python.py
+++ test_python.py
@@ -1,6 +1,19 @@
-tri = {}
-tri[0] = 0 
-tri[1] = 1
-tri[2] = 1
+import sys
+input = sys.stdin.readline
 
-print(tri)
+N = int(input())
+time = list(map(int, input().split()))
+young = mins = 0
+
+for t in time:
+    young += (t // 30 + 1) * 10
+    mins += (t // 60 + 1) * 15
+
+if mins == young:
+    print("Y M", mins)
+
+elif mins < young:
+    print("M", mins)
+
+else:
+    print("Y", young)
