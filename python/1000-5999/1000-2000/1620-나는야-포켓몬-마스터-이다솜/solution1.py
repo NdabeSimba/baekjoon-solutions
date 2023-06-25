8df9a6b566a26ebe7470657da7ecba02435fceb7
Juhun Park <riann3207@gmail.com>
2023-06-25 21:11:32 +0900

    1620

--- python/1000-5999/1000-2000/1620-나는야-포켓몬-마스터-이다솜/solution1.py
+++ python/1000-5999/1000-2000/1620-나는야-포켓몬-마스터-이다솜/solution1.py
@@ -6,14 +6,14 @@ N, M = map(int, input().split())
 poke_di = dict()
 poke_set = list()
 
-for i in range(1, N + 1):
+for i in range(N):
     name = input().strip()
     poke_di[name] = i
     poke_set.append(name)
 
 for i in range(M):
     find = input().strip()
-    if "0" <= find[0] <= "9":
+    try:
         print(poke_set[int(find) - 1])
-    else:
-        print(poke_di[find])
+    except:
+        print(poke_di[find] + 1)
