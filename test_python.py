acd02b06f4ef610b4d191b50f8e372e7886b3c0f
Juhun Park <riann3207@gmail.com>
2023-06-26 23:37:44 +0900

    9095-test

--- test_python.py
+++ test_python.py
@@ -1,19 +1,11 @@
 import sys
+import itertools
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
+N = int(input())
 
-poke_di = dict()
-poke_set = list()
-
-for i in range(1, N + 1):
-    name = input().strip()
-    poke_di[name] = i
-    poke_set.append(name)
-
-for i in range(M):
-    find = input().strip()
-    if "0" <= find[0] <= "9":
-        print(poke_set[int(find) - 1])
-    else:
-        print(poke_di[find])
+for _ in range(N):
+    num = int(input().strip())
+    arr = ['A', 'B', 'C']
+    nPr = itertools.permutations(arr, 2)
+    print(len(list(nPr)))
