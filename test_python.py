25860ae981076139f30c78eb6d691ca7b4880f5e
Juhun Park <riann3207@gmail.com>
2023-06-30 17:07:37 +0900

    2745

--- test_python.py
+++ test_python.py
@@ -1,27 +1,3 @@
 input = __import__('sys').stdin.readline
-
-N = int(input())
-C = int(input())
-
-graph = [[] for _ in range(N + 1)]
-
-for _ in range(C):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
-    
-count = 0
-visited = [False] * (N + 1)
-
-
-def DFS(n):
-    global count
-    visited[n] = True
-
-    for i in graph[n]:
-        if not visited[i]:
-            DFS(i)
-            count += 1
-
-DFS(1)
-print(count)
+a, b = input().strip().split()
+print(int(a, int(b)))
