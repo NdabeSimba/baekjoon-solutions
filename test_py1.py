003186ca27624a97b49e90ffc9d25404046be8d7
Juhun Park <riann3207@gmail.com>
2023-07-14 21:51:36 +0900

    bronze solution scratch

--- test_py1.py
+++ test_py1.py
@@ -12,3 +12,17 @@ for i in range(2, n + 1):
         dp[i] = min(dp[i], dp[i // 3] + 1)
 
 print(dp[n], dp)
+
+
+input = __import__("sys").stdin.readline
+N = int(input())
+num_list = list()
+
+num = 1
+temp = 6
+while num < N:
+    num_list.append(num)
+    num += temp
+    temp += 6
+
+print(len(num_list) + 1)
