a9dd271c37e02d53c4651e57dc61190db49f89f2
Juhun Park <riann3207@gmail.com>
2023-07-21 23:00:41 +0900

    2501

--- test_py2.py
+++ test_py2.py
@@ -1,19 +1,11 @@
-while True:
-    num1, num2 = map(int, input().split())
+N, K = map(int, input().split())
+num_list = list()
 
-    if num1 == num2 == 0:
-        break
-
-    if num1 > num2:
-        if num1 % num2 == 0:
-            print("multiple")
-        else:
-            print("neither")
-        continue
-
-    if num1 < num2:
-        if num2 % num1 == 0:
-            print("factor")
-        else:
-            print("neither")
-        continue
+for i in range(1, N + 1):
+    if N % i == 0:
+        num_list.append(i)
+        
+try:
+    print(num_list[K - 1])
+except:
+    print(0)
