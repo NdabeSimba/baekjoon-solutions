b5bcb717cbbece85884e50045bc8e96d744d12c2
Juhun Park <riann3207@gmail.com>
2023-08-21 17:26:33 -0400

    14425

--- python/11000-15999/14425-문자열-집합/main.py
+++ python/11000-15999/14425-문자열-집합/main.py
@@ -4,13 +4,17 @@ input = sys.stdin.readline
 N, M = map(int, input().split())
 
 S_set = set()
+string_list = list()
 result = 0
 
 for _ in range(N):
     S_set.add(input().strip())
 
+for _ in range(M):
+    string_list.append(input().strip())
+
 for i in range(M):
-    temp = input().strip()
+    temp = string_list.pop()
     if temp in S_set:
         result += 1
     else:
