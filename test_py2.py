c8e75a0be114ab82949a70d66a71dfa2ffc406b6
Juhun Park <riann3207@gmail.com>
2023-09-20 16:07:44 -0400

    working on 2346

--- test_py2.py
+++ test_py2.py
@@ -1,20 +1,19 @@
-N = int(input())
-have_list = sorted(list(map(int, input().split())))
+import sys
 
-M = int(input())
-comp_list = list(map(int, input().split()))
+input = sys.stdin.readline
 
+num = int(input())
+lis = list(map(int, input().split()))
 
-for num in comp_list:
-    start, end = 0, N - 1
-    result = False
-    while start <= end:
-        mid = (start + end) // 2
-        if have_list[mid] > num:
-            end = mid - 1
-        elif have_list[mid] < num:
-            start = mid + 1
-        else:
-            result = True
-            break
-    print(1 if result else 0, end=" ")
+popd = list()
+location = 0
+
+for i in range(num):
+    popd.append(lis.index(lis[location]) + 1)
+    temp = lis.pop(location)
+    location += temp
+    if location > num:
+        location = location % num
+    print(location)
+
+print(popd)
