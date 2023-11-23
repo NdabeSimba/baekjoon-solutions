0ad96f28d58454521828cee16f63dd6da7cd30cf
Juhun Park <riann3207@gmail.com>
2023-11-22 19:58:19 -0500

    zylab

---
+++ test3.py
@@ -0,0 +1,8 @@
+def add(x, y):
+    return x + y
+
+
+print('Begin test')
+s = add('hello', 5)
+print(s)
+print('End test')
--- test_py1.py
+++ test_py1.py
@@ -1,17 +1,32 @@
-import sys; input = sys.stdin.readline
+class Car:
+    def __init__(self):
+        self.model_year = 0
+        # TODO: Declare purchase_price attribute
+        self.purchase_price = 0
+        self.current_value = 0
 
-i, j = map(int, input().split())
-wd = dict()
+    def calc_current_value(self, current_year):
+        depreciation_rate = 0.15
+        # Car depreciation formula
+        car_age = current_year - self.model_year
+        self.current_value = round(self.purchase_price * (1 - depreciation_rate) ** car_age)
+    
+    # TODO: Define print_info() method to output model_year, purchase_price, and current_value
+    def print_info(self):
+        print(f'''Car's information:
+  Model year: {self.model_year}
+  Purchase price: ${self.purchase_price}
+  Current value: ${self.current_value}''')
 
-for i in range(i):
-    word = input().rstrip()
-    if len(word) >= j:
-        if word in wd:
-            wd[word] = wd[word] + 1
-        else:
-            wd[word] = 1
 
-temp = sorted(wd.items(), key=lambda x: (-x[1], -len(x[0]), x[0]))
-
-for k,v in temp:
-    print(k)
+if __name__ == "__main__":    
+    year = int(input()) 
+    price = int(input())
+    current_year = int(input())
+    
+    my_car = Car()
+    my_car.model_year = year
+    my_car.purchase_price = price
+    my_car.calc_current_value(current_year)
+    my_car.print_info()
+    
