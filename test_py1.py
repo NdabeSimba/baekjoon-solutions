199bb68110bb67f70029814c575169fe0216028b
Juhun Park <riann3207@gmail.com>
2023-12-07 20:56:22 -0500

    formatter

--- test_py1.py
+++ test_py1.py
@@ -1,5 +1,6 @@
 N = int(input())
 
+
 def recursion(s, l, r, cnt):
     if (l >= r):
         return 1, cnt
@@ -7,9 +8,11 @@ def recursion(s, l, r, cnt):
         return 0, cnt
     else:
         return recursion(s, l + 1, r - 1, cnt + 1)
-    
+
+
 def is_palindrome(s):
     return recursion(s, 0, len(s) - 1, 1)
+
 
 for i in range(N):
     print(*is_palindrome(input()))
