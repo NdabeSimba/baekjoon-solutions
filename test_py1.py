e3a8d48cd81c497662012eeb4ac6ba82a6f25025
Juhun Park <riann3207@gmail.com>
2024-01-03 03:53:30 -0500

    23971

--- test_py1.py
+++ test_py1.py
@@ -1,12 +1,8 @@
-N, K = map(int, input().split())
-yos_lis = list()
-num_lis = list(i for i in range(1, N + 1))
-ind = 0
+import math
 
-for _ in range(N):
-    ind += K - 1
-    if ind >= len(num_lis):
-        ind = ind % len(num_lis)
-    yos_lis.append(num_lis.pop(ind))
+H, W, N, M = map(int, input().split())
 
-print(str(yos_lis).replace('[','<').replace(']','>'))
+Hc = math.ceil(H / (N + 1))
+Wc = math.ceil(W / (M + 1))
+
+print(Hc * Wc)
