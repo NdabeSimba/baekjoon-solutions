6dd19b33c1187a39d20389696d1756c10a9b2345
Juhun Park <riann3207@gmail.com>
2024-01-05 09:39:57 -0500

    1145 test

--- test_py1.py
+++ test_py1.py
@@ -1,8 +1,19 @@
-import math
+num_list = sorted(list(map(int, input().split())))
+r = len(num_list)
+temp = num_list[0]
 
-H, W, N, M = map(int, input().split())
+while True:
+    res = 0
 
-Hc = math.ceil(H / (N + 1))
-Wc = math.ceil(W / (M + 1))
+    for i in range(r):
+        if temp / num_list[i] == 0:
+            res += 1
 
-print(Hc * Wc)
+    if res == 3:
+        break
+    else:
+        temp += 1
+
+    print(temp, res)
+
+print(temp)
